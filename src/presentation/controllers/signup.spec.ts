1743514229dc743215e97ba0ea62c5df067725e1
Geverson Loch <geverson.loch@uniodigital.com.br>
2022-08-10 09:01:49 -0300

    test: ensure SignUpController return an error if no e-mail is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,7 +2,9 @@ import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Deve retornar 400 se nenhum nome for fornecido', () => {
+
     const sut = new SignUpController()
+
     const httpRequest = {
       body: {
         email: 'any_email',
@@ -10,31 +12,27 @@ describe('SignUp Controller', () => {
         passwordConfirmation: 'any_password'
       }
     }
+
     const httpResponse = sut.handle(httpRequest)
+
     /*
       NOTE: toBe
       Verifica se um valor é o que você espera.
     */
     expect(httpResponse.statusCode).toBe(400)
+
     /*
       NOTE: toEqual
       Usado quando você deseja verificar se dois objetos têm o mesmo valor.
       Esse matcher verifica recursivamente a igualdade de todos os campos, em vez de verificar a identidade do objeto.
     */
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
-  })
-
-  test('Deve retornar 400 se nenhum e-mail for fornecido', () => {
-    const sut = new SignUpController()
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: e-mail'))
+    
   })
 })
+
+/* 
+12:20
+Pasta 3. SignUp API - Presentation Layer
+Aula 1
+*/
