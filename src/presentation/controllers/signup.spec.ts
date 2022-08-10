94ac08dd9112b4a2d1f12149e5a27e11699323ee
Geverson Loch <geverson.loch@uniodigital.com.br>
2022-08-10 08:38:57 -0300

    test: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,6 +14,25 @@ describe('SignUp Controller', () => {
     }
 
     const httpResponse = sut.handle(httpRequest)
+
+    /*
+      NOTE: toBe
+      Verifica se um valor é o que você espera.
+    */
     expect(httpResponse.statusCode).toBe(400)
+
+    /*
+      NOTE: toEqual
+      Usado quando você deseja verificar se dois objetos têm o mesmo valor.
+      Esse matcher verifica recursivamente a igualdade de todos os campos, em vez de verificar a identidade do objeto.
+    */
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    
   })
 })
+
+/* 
+12:20
+Pasta 3. SignUp API - Presentation Layer
+Aula 1
+*/
