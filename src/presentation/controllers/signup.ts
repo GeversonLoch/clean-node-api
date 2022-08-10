6f0773e22162e66bb7f13ccee06fdf467f1dc554
Geverson Loch <geverson.loch@uniodigital.com.br>
2022-08-10 09:09:33 -0300

    feat: ensure SignUpController return an error if no e-mail is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,19 @@
 export class SignUpController {
   handle (httpReques: any): any  {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+
+    if (!httpReques.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: e-mail')
+      }
     }
+
+    if (!httpReques.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
   }
 }
