9789409e37cd87e0b1bf1ad4aff03b95e54e4712
Geverson Loch <geverson.loch@uniodigital.com.br>
2023-11-02 16:09:59 -0300

    refactor: add Controller interface to SignUpController

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,14 +1,15 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
-  handle (httpReques: HttpRequest): HttpResponse  {
+export class SignUpController implements Controller {
+  handle (httpRequest: HttpRequest): HttpResponse {
 
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const fild of requiredFields) {
-      if (!httpReques.body[fild]) {
+      if (!httpRequest.body[fild]) {
         return badRequest(new MissingParamError(fild))
       }
     }
