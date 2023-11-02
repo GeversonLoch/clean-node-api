0286adbf10635d136de534a55590f8cf811a9d84
Geverson Loch <geverson.loch@uniodigital.com.br>
2023-11-02 15:57:25 -0300

    test: fix typo in password confirmation error message

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -54,7 +54,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  test('Should return 400 if no passwordConfirmation is provided', () => {
+  test('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
