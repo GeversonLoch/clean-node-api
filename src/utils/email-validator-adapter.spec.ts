c67b3ea7516fc4576f8b27c2f2766e813ae2c785
Geverson Loch <geverson.loch@uniodigital.com.br>
2023-12-28 09:51:52 -0300

    test: ensure EmailValidatorAdapter return false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,12 @@
+import { EmailValidatorAdapter } from './email-validator'
+
+describe('Email Validator Adapter', () => {
+
+    test('Should return false if validator returns false', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('invalid_email@email.com')
+
+        expect(isValid).toBe(false)
+    })
+
+})
