f01454ee0ec9946c0dfd4c327d44e1f581b6530f
Geverson Loch <geverson.loch@uniodigital.com.br>
2023-12-28 10:56:22 -0300

    docs: add documentation in email-validator-adapter.spec.ts

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,5 +1,16 @@
 import { EmailValidatorAdapter } from './email-validator'
 
+/*
+Ao mockar o retorno do método isEmail, podemos definir manualmente se ele deve retornar true ou false,
+dependendo do cenário de teste que estamos simulando.
+Isso nos permite testar diferentes caminhos de execução do código que utiliza o método isEmail,
+sem depender do comportamento real da biblioteca validator.
+
+Em resumo, o mock do retorno do método isEmail permite que tenhamos controle total sobre o comportamento desse método durante os testes,
+facilitando a criação de cenários de teste específicos e garantindo a confiabilidade dos testes unitários.
+
+Por padrão o retorno mockado deve ser sempre verdadeiro.
+*/
 jest.mock('validator', () => ({
     isEmail(): boolean {
         return true
@@ -8,6 +19,7 @@ jest.mock('validator', () => ({
 
 describe('Email Validator Adapter', () => {
 
+    // Deve retornar falso se o validador retornar falso
     test('Should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter()
         jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
@@ -16,6 +28,7 @@ describe('Email Validator Adapter', () => {
         expect(isValid).toBe(false)
     })
 
+    // Deve retornar verdadeiro se o validador retornar verdadeiro
     test('Should return true if validator returns true', () => {
         const sut = new EmailValidatorAdapter()
         const isValid = sut.isValid('valid_email@email.com')
