1611d77370006e39c33afe56d2c852d91b6e1358
Geverson Loch <geverson.loch@uniodigital.com.br>
2023-12-28 10:53:06 -0300

    test: ensure EmailValidatorAdapter return true if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,12 +1,26 @@
 import { EmailValidatorAdapter } from './email-validator'
 
+jest.mock('validator', () => ({
+    isEmail(): boolean {
+        return true
+    }
+}))
+
 describe('Email Validator Adapter', () => {
 
     test('Should return false if validator returns false', () => {
         const sut = new EmailValidatorAdapter()
+        jest.spyOn(sut, 'isValid').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid_email@email.com')
 
         expect(isValid).toBe(false)
+    })
+
+    test('Should return true if validator returns true', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('valid_email@email.com')
+
+        expect(isValid).toBe(true)
     })
 
 })
