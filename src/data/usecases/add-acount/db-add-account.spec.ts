e4dd70d3173e78cc184acbe345d3c4906c028cbe
Geverson Loch <geverson.loch@uniodigital.com.br>
2024-02-14 11:11:19 -0300

    test: ensure DbAddAccount throw if Encripter throws

--- src/data/usecases/add-acount/db-add-account.spec.ts
+++ src/data/usecases/add-acount/db-add-account.spec.ts
@@ -52,4 +52,24 @@ describe('DbAddAccount Usecase', () => {
         // Verifica se o espião encrypterSpy foi chamado com a senha correta ('valid_password')
         expect(encrypterSpy).toHaveBeenCalledWith('valid_password')
     })
+
+    // Teste para garantir que o erro seja tratado na controller
+    test('Should throw if Encripter throws', async () => {
+
+        const { sut, encrypterStub } = makeSut()
+
+        // Cria um espião (spy) no método encrypt do encrypterStub
+        jest.spyOn(encrypterStub, 'encrypt').mockReturnValueOnce(Promise.reject(new Error()))
+
+        // Chama o método add da instância DbAddAccount
+        const promise = sut.add({
+            name: 'valid_name',
+            email: 'valid_email',
+            password: 'valid_password'
+        })
+
+        // Verifica se o método add lançou uma exceção
+        await expect(promise).rejects.toThrow()
+    })
+
 })
