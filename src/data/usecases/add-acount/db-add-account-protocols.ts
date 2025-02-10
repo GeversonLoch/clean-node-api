aad5dbbb92b8334e0616a2024a8ba1b886df3491
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-02-10 18:00:01 -0300

    refactor: rename interfaces files with default

--- src/data/usecases/add-acount/db-add-account-protocols.ts
+++ src/data/usecases/add-acount/db-add-account-protocols.ts
@@ -1,5 +1,5 @@
 export * from '../../../domain/usecases/add-account'
 export * from '../../../domain/models/account'
-export * from '../../protocols/IEncrypter'
-export * from '../../protocols/IAddAccountRepository'
+export * from '../../protocols/encrypter'
+export * from '../../protocols/add-account-repository'
 
