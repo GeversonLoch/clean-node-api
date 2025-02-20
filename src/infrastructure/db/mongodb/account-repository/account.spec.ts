5746c8ea27492e7b82f542c9d1c842595c8dd084
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-02-20 17:59:52 -0300

    test: add AccountMongoRepository tests

---
+++ src/infrastructure/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,34 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+let mongoHelper: MongoHelper
+
+beforeAll(async () => {
+  mongoHelper = new MongoHelper(process.env.MONGO_URL, 'jest')
+  await mongoHelper.connect()
+})
+
+beforeEach(async () => {
+  const accountCollection = mongoHelper.getCollection('accounts')
+  await accountCollection.deleteMany({})
+})
+
+afterAll(async () => {
+  await mongoHelper.disconnect()
+})
+
+describe("Account Mongo Repository", () => {
+  test("Should return an account on success", async () => {
+    const sut = new AccountMongoRepository(mongoHelper)
+    const account = await sut.add({
+      name: "valid_name",
+      email: "valid_email@email.com",
+      password: "hashed_password",
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe("valid_name")
+    expect(account.email).toBe("valid_email@email.com")
+    expect(account.password).toBe("hashed_password")
+  })
+})
