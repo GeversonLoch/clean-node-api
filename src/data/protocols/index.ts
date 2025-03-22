48e712b11d16e251a2267101050bb039a211eabb
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-03-22 12:27:49 -0300

    feat: add LogErrorRepository interface and implement error logging in LogControllerDecorator

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,3 +1,4 @@
 export * from "./add-account-repository.interface"
 export * from "./encrypter.interface"
 export * from "./database-adapter.interface"
+export * from "./log-error-repository.interface"
--- src/main/decorators/log-controller-decorator.ts
+++ src/main/decorators/log-controller-decorator.ts
@@ -1,18 +1,26 @@
 /*
-    ???
+* Decorador responsável por adicionar a 
+* funcionalidade de log de erros ao controlador original.
 */
 
 import { IController, IHttpRequest, IHttpResponse } from "@presentation/protocols"
+import { ILogErrorRepository } from "@data/protocols"
 
 export class LogControllerDecorator implements IController {
     private readonly controller: IController
+    private readonly logErrorRepository: ILogErrorRepository
 
-    constructor(controller: IController) {
+    constructor(controller: IController, logErrorRepository: ILogErrorRepository) {
         this.controller = controller
+        this.logErrorRepository = logErrorRepository
     }
 
+    // Método que trata a requisição e, caso ocorra um erro interno, registra o log do erro.
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         const httpResponse = await this.controller.handle(httpRequest)
+        if (httpResponse.statusCode === 500) {
+            await this.logErrorRepository.log(httpResponse.body.stack)
+        }
         return httpResponse
     }
 }
