20da7618baced6501c91184d15fa353b774b821b
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-21 17:14:19 -0300

    test: adicionar teste para garantir que DbAuthentication lança exceção ao falhar no repositório

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -40,4 +40,14 @@ describe('DbAuthentication Usecase', () => {
         await sut.auth(makeFakeAuthentication())
         expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
+
+    // Garante que DbAuthentication lance uma exceção se o LoadAccountByEmailRepository lançar
+    test('Should throw if LoadAccountByEmailRepository throws', async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+        jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(
+            Promise.reject(new Error())
+        )
+        const promise = sut.auth(makeFakeAuthentication())
+        await expect(promise).rejects.toThrow()
+    })
 })
