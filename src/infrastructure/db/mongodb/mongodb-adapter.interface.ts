2851a64d88dc51fbcc612e527cf71374937247f1
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-22 08:34:12 -0300

    refactor: move data layer interfaces to folders

--- src/infrastructure/db/mongodb/mongodb-adapter.interface.ts
+++ src/infrastructure/db/mongodb/mongodb-adapter.interface.ts
@@ -1,7 +1,7 @@
-import { IDatabaseAdapter } from "@data/protocols/database-adapter.interface"
-import { Collection, WithId, Document } from "mongodb"
+import { IDatabaseAdapter } from '@data/protocols'
+import { Collection, WithId, Document } from 'mongodb'
 
 export interface IMongoDBAdapter extends IDatabaseAdapter {
-  getCollection<T extends Document>(name: string): Promise<Collection<T>>
-  map<T extends Document>(document: WithId<T> | null)
+    getCollection<T extends Document>(name: string): Promise<Collection<T>>
+    map<T extends Document>(document: WithId<T> | null)
 }
