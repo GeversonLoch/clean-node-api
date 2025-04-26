122fc8ad840f94441cee1893ed1d60e7e0c2a406
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-26 19:35:26 -0300

    feat: adicionar suporte ao gerador de tokens na autenticação

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,14 +1,20 @@
-import { IHashComparer, ILoadAccountByEmailRepository } from '@data/protocols'
+import { IHashComparer, ILoadAccountByEmailRepository, ITokenGenerator } from '@data/protocols'
 import { IAuthenticationModel } from '@domain/models'
 import { IAuthentication } from '@domain/usecases'
 
 export class DbAuthentication implements IAuthentication {
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
     private readonly hashComparer: IHashComparer
+    private readonly tokenGenerator: ITokenGenerator
 
-    constructor(loadAccountByEmailRepository: ILoadAccountByEmailRepository, hashComparer: IHashComparer) {
+    constructor(
+        loadAccountByEmailRepository: ILoadAccountByEmailRepository,
+        hashComparer: IHashComparer,
+        tokenGenerator: ITokenGenerator,
+    ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashComparer = hashComparer
+        this.tokenGenerator = tokenGenerator
     }
 
     async auth(authentication: IAuthenticationModel): Promise<string> {
@@ -16,6 +22,7 @@ export class DbAuthentication implements IAuthentication {
         const account = await this.loadAccountByEmailRepository.load(email)
         if (account) {
             await this.hashComparer.compare(password, account.password)
+            await this.tokenGenerator.generate(account.id)
         }
         return null
     }
