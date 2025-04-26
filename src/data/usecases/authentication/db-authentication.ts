a701a6a41a33fafb2fcd755570a48bafdfb80988
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-26 20:16:04 -0300

    feat: adicionar atualização de token de acesso no DbAuthentication

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -1,4 +1,4 @@
-import { IHashComparer, ILoadAccountByEmailRepository, ITokenGenerator } from '@data/protocols'
+import { IHashComparer, ILoadAccountByEmailRepository, ITokenGenerator, IUpdateAccessTokenRepository } from '@data/protocols'
 import { IAuthenticationModel } from '@domain/models'
 import { IAuthentication } from '@domain/usecases'
 
@@ -6,15 +6,18 @@ export class DbAuthentication implements IAuthentication {
     private readonly loadAccountByEmailRepository: ILoadAccountByEmailRepository
     private readonly hashComparer: IHashComparer
     private readonly tokenGenerator: ITokenGenerator
+    private readonly updateAccessTokenRepository: IUpdateAccessTokenRepository
 
     constructor(
         loadAccountByEmailRepository: ILoadAccountByEmailRepository,
         hashComparer: IHashComparer,
         tokenGenerator: ITokenGenerator,
+        updateAccessTokenRepository: IUpdateAccessTokenRepository,
     ) {
         this.loadAccountByEmailRepository = loadAccountByEmailRepository
         this.hashComparer = hashComparer
         this.tokenGenerator = tokenGenerator
+        this.updateAccessTokenRepository = updateAccessTokenRepository
     }
 
     async auth(authentication: IAuthenticationModel): Promise<string> {
@@ -24,6 +27,7 @@ export class DbAuthentication implements IAuthentication {
             const isValid = await this.hashComparer.compare(password, account.password)
             if (isValid) {
                 const accessToken = await this.tokenGenerator.generate(account.id)
+                await this.updateAccessTokenRepository.update(account.id, accessToken)
                 return accessToken
             }
         }
