7ef2dcf81adf22e89ee964658677d8adb7393d7d
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-29 09:07:35 -0300

    test: adicionar teste para garantir que o método compare retorne false em caso de falha

--- src/infrastructure/cryptography/bcrypt-adapter.spec.ts
+++ src/infrastructure/cryptography/bcrypt-adapter.spec.ts
@@ -66,4 +66,12 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
   })
+
+  // Garante que o método compare retorne false quando a comparação falhar
+  test('Should return true when compare fails', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => Promise.resolve(false));
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
 })
