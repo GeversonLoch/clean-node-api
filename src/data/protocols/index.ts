2f3505fe21b4870afc317f70d0411d5f49a03e08
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-05-01 20:14:24 -0300

    refactor: mover interfaces de repositório para a pasta correta

--- src/data/protocols/index.ts
+++ src/data/protocols/index.ts
@@ -1,8 +1,8 @@
 export * from './cryptography/hasher.interface'
 export * from './cryptography/hash-comparer.interface'
 export * from './cryptography/encrypter.interface'
-export * from './db/add-account-repository.interface'
+export * from './db/account/add-account-repository.interface'
 export * from './db/log-error-repository.interface'
-export * from './db/load-account-by-email-repository.interface'
-export * from './db/update-access-token-repository.interface'
+export * from './db/account/load-account-by-email-repository.interface'
+export * from './db/account/update-access-token-repository.interface'
 export * from './db/database-adapter.interface' // TODO: Validar se esse adapter realmente pertence a camada data!
