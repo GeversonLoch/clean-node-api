c51d34957420a8600d47b977f9696e910705bbb6
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-05-01 15:17:27 -0300

    refactor: atualizar a documentação do repositório AccountMongoRepository para maior clareza

--- src/infrastructure/db/mongodb/account-repository/account.ts
+++ src/infrastructure/db/mongodb/account-repository/account.ts
@@ -1,7 +1,11 @@
 /*
-* Implementação do repositório de Accounts utilizando o MongoDB.
-* A classe AccountMongoRepository adere à interface IAddAccountRepository e é responsável
-* por persistir os dados de uma conta no banco, utilizando um helper/adapter para lidar com operações do MongoDB.
+* Repositório MongoDB para gerenciamento de contas.
+*
+* Esta implementação de AccountMongoRepository adere às interfaces
+* IAddAccountRepository e ILoadAccountByEmailRepository e é responsável
+* por adicionar novas contas e obter contas na coleção 'accounts' do MongoDB.
+* Utiliza IMongoDBAdapter para realizar operações
+* de conexão, obtenção de coleção e mapeamento de documentos.
 */
 
 import { IAddAccountRepository, ILoadAccountByEmailRepository } from "@data/protocols"
