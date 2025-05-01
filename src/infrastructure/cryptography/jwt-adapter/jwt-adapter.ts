e020f3ee51274a7bd450f0fa8ed0871bb019e8f2
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-04-30 21:38:32 -0300

    feat: adicionar JwtAdapter para gerenciamento de tokens

---
+++ src/infrastructure/cryptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken'
+import { IEncrypter } from '@data/protocols'
+
+export class JwtAdapter implements IEncrypter {
+    private readonly secret: string
+
+    constructor(secret: string) {
+        this.secret = secret
+    }
+
+    async encrypt(value: string): Promise<string> {
+        const accessToken = await jwt.sign({ id: value }, this.secret)
+        return accessToken
+    }
+}
