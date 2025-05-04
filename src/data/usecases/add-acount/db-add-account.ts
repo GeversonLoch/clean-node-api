5c5ddf6ca66da43bbb90664c46db2c16608ade26
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-05-04 09:05:30 -0300

    refactor: simplifica o construtor da classe DbAddAccount

--- src/data/usecases/add-acount/db-add-account.ts
+++ src/data/usecases/add-acount/db-add-account.ts
@@ -3,14 +3,10 @@ import { IAccountModel, IAddAccountModel } from "@domain/models"
 import { IAddAccount } from "@domain/usecases"
 
 export class DbAddAccount implements IAddAccount {
-
-    private readonly hasher: IHasher
-    private readonly addAccountRepository: IAddAccountRepository
-
-    constructor (hasher: IHasher, addAccountRepository: IAddAccountRepository) {
-        this.hasher = hasher
-        this.addAccountRepository = addAccountRepository
-    }
+    constructor (
+        private readonly hasher: IHasher,
+        private readonly addAccountRepository: IAddAccountRepository
+    ) {}
 
     async add(accountData: IAddAccountModel): Promise<IAccountModel> {
         const hashed_password = await this.hasher.hash(accountData.password)
