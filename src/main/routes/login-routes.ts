67f1f36db6fe63882a367b3ce61705ee6fe435d2
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-05-05 20:44:06 -0300

    refactor: corrige a documentação das rotas de login e signup

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,11 +1,13 @@
 /**
- * Define as rotas relacionadas ao cadastro de usuários.
- */
+* Define as rotas relacionadas ao cadastro de usuários.
+*/
 
 import { Router } from 'express'
 import { expressRouteAdapter } from '@main/adapters/express/express-route-adapter'
 import { makeSignUpController } from '@main/factories/signup/signup-factory'
+import { makeLoginController } from '@main/factories/login/login-factory'
 
 export default (router: Router): void => {
     router.post('/signup', expressRouteAdapter(makeSignUpController()))
+    router.post('/login', expressRouteAdapter(makeLoginController()))
 }
