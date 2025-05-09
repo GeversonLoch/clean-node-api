8fb252a42ed94c5e69c4ec214be26910fde1dc3e
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-05-08 22:04:20 -0300

    refactor: atualiza o processo de build e roteamento

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -2,16 +2,20 @@
 * Arquivo responsável por centralizar o carregamento dinâmico das rotas da aplicação.
 * Utiliza o fast-glob para localizar e importar automaticamente todos os arquivos de rotas.
 */
-import { Express, Router } from "express"
-import fg from "fast-glob" // Importa o fast-glob para realizar buscas de arquivos de forma eficiente
+import { Express, Router } from 'express'
+import fg from 'fast-glob'
+import { resolve } from 'path'
 
 export default (app: Express): void => {
     const router = Router()
-    app.use('/api', router) // Registra o router com o prefixo '/api', agrupando todas as rotas da API
 
-    // Procura por todos os arquivos que terminam com "routes.ts" dentro de "src/main/routes"
-    fg.sync('**/src/main/routes/**routes.ts').map(async file => {
-        // Importa cada arquivo de rota encontrado e executa sua função padrão, passando o router
-        (await import(`../../../${file}`)).default(router)
+    // Registra o router com o prefixo '/api', agrupando todas as rotas da API
+    app.use('/api', router)
+
+    const path = resolve(__dirname, '../routes/')
+    const source = fg.convertPathToPattern(path)
+
+    fg.sync(`${source}/**routes.{js,ts}`, { absolute: true }).map(async file => {
+        (await import(file)).default(router)
     })
 }
