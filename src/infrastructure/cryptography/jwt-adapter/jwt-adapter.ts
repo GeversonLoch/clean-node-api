31f997f3ffcb3518a62feac1051a6f518afe6cab
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-06-08 19:07:18 -0300

    fix: ajusta a implementação do método decrypt na classe JwtAdapter para retornar o valor decifrado

--- src/infrastructure/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infrastructure/cryptography/jwt-adapter/jwt-adapter.ts
@@ -9,8 +9,8 @@ export class JwtAdapter implements IEncrypter, IDecrypter {
         return accessToken
     }
 
-    async decrypt(value: string): Promise<string> {
-        await jwt.verify(value, this.secret)
-        return null
+    async decrypt(token: string): Promise<string> {
+        const value: any = await jwt.verify(token, this.secret)
+        return value // TODO: Rever esse retorno forçado com any
     }
 }
