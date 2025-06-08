21217911a8ed5318ce49e20b17eb81143f3f799b
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-06-08 10:52:25 -0300

    feat: adiciona tratamento de erro e reorganiza lógica no AuthMiddleware

--- src/presentation/middlewares/authentication/auth-middleware.ts
+++ src/presentation/middlewares/authentication/auth-middleware.ts
@@ -1,21 +1,25 @@
 import { ILoadAccountByToken } from '@domain/usecases'
 import { AccessDeniedError } from '@presentation/errors'
-import { forbidden, success } from '@presentation/helpers'
+import { forbidden, internalServerError, success } from '@presentation/helpers'
 import { IHttpRequest, IHttpResponse, IMiddleware } from '@presentation/protocols'
 
 export class AuthMiddleware implements IMiddleware {
     constructor(private readonly loadAccountByToken: ILoadAccountByToken) {}
 
     async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-        const accessToken = httpRequest.headers?.['x-access-token']
-        if (accessToken) {
-            const account = await this.loadAccountByToken.loadByToken(accessToken)
-            if (account) {
-                return success({
-                    accountId: account.id,
-                })
+        try {
+            const accessToken = httpRequest.headers?.['x-access-token']
+            if (accessToken) {
+                const account = await this.loadAccountByToken.loadByToken(accessToken)
+                if (account) {
+                    return success({
+                        accountId: account.id,
+                    })
+                }
             }
+            return forbidden(new AccessDeniedError())
+        } catch (error) {
+            return internalServerError(error)
         }
-        return forbidden(new AccessDeniedError())
     }
 }
