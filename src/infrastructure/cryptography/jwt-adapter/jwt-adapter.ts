96ee0a631f3bd534b0230989e9bf660567e00117
Geverson Loch <geverson.loch@uniodigital.com.br>
2025-06-08 18:45:49 -0300

    feat: adiciona a interface IDecrypter à classe JwtAdapter e implementa o método decrypt

--- src/infrastructure/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infrastructure/cryptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,16 @@
 import jwt from 'jsonwebtoken'
-import { IEncrypter } from '@data/protocols'
+import { IDecrypter, IEncrypter } from '@data/protocols'
 
-export class JwtAdapter implements IEncrypter {
+export class JwtAdapter implements IEncrypter, IDecrypter {
     constructor(private readonly secret: string) {}
 
     async encrypt(value: string): Promise<string> {
         const accessToken = await jwt.sign({ id: value }, this.secret)
         return accessToken
+    }
+
+    async decrypt(value: string): Promise<string> {
+        await jwt.verify(value, this.secret)
+        return null
     }
 }
