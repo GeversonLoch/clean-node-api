47e082d2ed1ad907ce6c244b8348f638ce50fd9a
Geverson Loch <geverson.loch@yunidigital.com.br>
2025-08-04 08:09:33 -0300

    refactor: simplify authentication middleware usage in survey routes

--- src/main/routes/survey-routes.ts
+++ src/main/routes/survey-routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
 import { expressRouteAdapter } from '@main/adapters/express/express-route-adapter'
-import { expressMiddlewareAdapter } from '@main/adapters/express/express-middleware-adapter'
-import { makeAuthMiddleware } from '@main/factories/middlewares/auth-middlewares-factory'
+import { auth, adminAuth } from '@main/middlewares/auth'
 import { makeAddSurveyController } from '@main/factories/controllers/survey/add-survey/add-survey-controller-factory'
 import { makeLoadSurveysController } from '@main/factories/controllers/survey/load-surveys/load-surveys-controller-factory'
 
 export default (router: Router): void => {
-    const auth = expressMiddlewareAdapter(makeAuthMiddleware())
-    const adminAuth = expressMiddlewareAdapter(makeAuthMiddleware('admin'))
     router.post('/add-survey', adminAuth, expressRouteAdapter(makeAddSurveyController()))
-    router.post('/surveys', auth, expressRouteAdapter(makeLoadSurveysController()))
+    router.get('/surveys', auth, expressRouteAdapter(makeLoadSurveysController()))
 }
