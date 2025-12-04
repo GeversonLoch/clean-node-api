e6fd148eaac9efd23d0b3408d1e8368fc2a5e4a4
Geverson Loch <geversonloch@gmail.com>
2025-12-03 22:48:29 -0300

    chore(deps): fix dependency-cruiser config path

---
+++ dependency-cruiser.config.js
@@ -0,0 +1,29 @@
+/**
+ * Dependency-cruiser configuration.
+ * See https://github.com/sverweij/dependency-cruiser/blob/main/doc/configuration.md
+ */
+module.exports = {
+  extends: 'dependency-cruiser/configs/recommended-strict',
+  options: {
+    includeOnly: ['^src'],
+    doNotFollow: {
+      path: 'node_modules',
+    },
+    tsConfig: {
+      fileName: './tsconfig.json',
+    },
+    alias: {
+      '^@domain/(.*)': 'src/domain/$1',
+      '^@data/(.*)': 'src/data/$1',
+      '^@infrastructure/(.*)': 'src/infrastructure/$1',
+      '^@presentation/(.*)': 'src/presentation/$1',
+      '^@utils/(.*)': 'src/utils/$1',
+      '^@main/(.*)': 'src/main/$1',
+    },
+    reporterOptions: {
+      dot: {
+        collapsePattern: '^(domain|data|infrastructure|presentation|utils|main)\\/',
+      },
+    },
+  },
+};
