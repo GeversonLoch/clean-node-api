3e402a4fd5c75dae97a2462ae3ad3afe167b7a8b
Geverson Loch <geversonloch@gmail.com>
2025-12-03 22:02:19 -0300

    chore: add dependency cruiser configuration

---
+++ dependency-cruiser.config.js
@@ -0,0 +1,30 @@
+/**
+ * Dependency-cruiser configuration.
+ * See https://github.com/sverweij/dependency-cruiser/blob/main/doc/configuration.md
+ */
+module.exports = {
+  extends: 'dependency-cruiser/configs/recommended-strict',
+  options: {
+    baseDir: 'src',
+    includeOnly: ['^src'],
+    doNotFollow: {
+      path: 'node_modules',
+    },
+    tsConfig: {
+      fileName: './tsconfig.json',
+    },
+    alias: {
+      '^@domain/(.*)': 'src/domain/$1',
+      '^@data/(.*)': 'src/data/$1',
+      '^@infrastructure/(.*)': 'src/infrastructure/$1',
+      '^@presentation/(.*)': 'src/presentation/$1',
+      '^@utils/(.*)': 'src/utils/$1',
+      '^@main/(.*)': 'src/main/$1',
+    },
+    reporterOptions: {
+      dot: {
+        collapsePattern: '^(domain|data|infrastructure|presentation|utils|main)\\/',
+      },
+    },
+  },
+};
